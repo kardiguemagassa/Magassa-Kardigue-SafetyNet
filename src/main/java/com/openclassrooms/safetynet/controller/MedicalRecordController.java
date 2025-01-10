e6bd455d44e6b461384cee6e3935d2ff17d1d517
kardiguemagassa <magassakara@gmail.com>
2025-01-10 19:06:07 +0100

    Update Endpoint MedicalRecordController.java

--- src/main/java/com/openclassrooms/safetynet/controller/MedicalRecordController.java
+++ src/main/java/com/openclassrooms/safetynet/controller/MedicalRecordController.java
@@ -2,6 +2,7 @@ package com.openclassrooms.safetynet.controller;
 
 import com.openclassrooms.safetynet.dto.MedicalRecordDTO;
 import com.openclassrooms.safetynet.service.MedicalRecordService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,46 +11,44 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@AllArgsConstructor
 public class MedicalRecordController {
 
     private final MedicalRecordService medicalRecordService;
 
-    public MedicalRecordController(MedicalRecordService medicalRecordService) {
-        this.medicalRecordService = medicalRecordService;
-    }
-
-    @GetMapping("/medicalrecord")
+    @GetMapping("/medicalRecord")
     public ResponseEntity<List<MedicalRecordDTO>> getMedicalRecord () {
         List<MedicalRecordDTO> medicalRecords = medicalRecordService.getMedicalRecords();
         return new ResponseEntity<>(medicalRecords, HttpStatus.OK);
         //http://localhost:8080/medicalrecord
     }
 
-    @PostMapping("/medicalrecord/saveAll")
+    @PostMapping("/medicalRecord")
     public ResponseEntity<List<MedicalRecordDTO>> saveAll (@RequestBody List<MedicalRecordDTO> medicalRecords) {
         List<MedicalRecordDTO> savedMedicalRecords = medicalRecordService.saveAll(medicalRecords);
-        return new ResponseEntity<>(savedMedicalRecords, HttpStatus.OK);
-        //http://localhost:8080/medicalrecord/saveAll
+        return new ResponseEntity<>(savedMedicalRecords, HttpStatus.CREATED);
+        //http://localhost:8080/medicalrecord
     }
-
-    @PostMapping("/medicalrecord/save")
+    /*
+    @PostMapping("/medicalRecord")
     public ResponseEntity<MedicalRecordDTO> save (@RequestBody MedicalRecordDTO medicalRecord) {
         MedicalRecordDTO savedMedicalRecord = medicalRecordService.save(medicalRecord);
         return new ResponseEntity<>(savedMedicalRecord, HttpStatus.OK);
-        // http://localhost:8080/medicalrecord/save
+        // http://localhost:8080/medicalrecord
     }
+     */
 
-    @PutMapping("/medicalrecord/update")
+    @PutMapping("/medicalRecord")
     public ResponseEntity<MedicalRecordDTO> update (@RequestBody MedicalRecordDTO medicalRecord) {
         Optional<MedicalRecordDTO> updatedMedicalRecord = medicalRecordService.update(medicalRecord);
         return new ResponseEntity<>(updatedMedicalRecord.get(), HttpStatus.OK);
-        // http://localhost:8080/medicalrecord/update
+        // http://localhost:8080/medicalrecord
     }
 
-    @DeleteMapping("/medicalrecord/delete")
+    @DeleteMapping("/medicalRecord")
     public ResponseEntity<Boolean> delete (@RequestParam String firstName, @RequestParam String lastName) {
-        medicalRecordService.deleteByFullName(firstName, lastName);
-        return new ResponseEntity<>(true, HttpStatus.OK);
-        // http://localhost:8080/medicalrecord/delete?firstName=John&lastName=Boyd
+        boolean deleted = medicalRecordService.deleteByFullName(firstName, lastName);
+        return new ResponseEntity<>(deleted, HttpStatus.OK);
+        // http://localhost:8080/medicalrecord?firstName=John&lastName=Boyd
     }
 }
