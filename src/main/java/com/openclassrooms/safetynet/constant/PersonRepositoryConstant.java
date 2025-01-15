5b34c1bbc304fe4033c4a01f2d43b31addb86464
kardiguemagassa <magassakara@gmail.com>
2025-01-15 12:44:00 +0100

    Add constant in personRepository

--- src/main/java/com/openclassrooms/safetynet/constant/PersonRepositoryConstant.java
+++ src/main/java/com/openclassrooms/safetynet/constant/PersonRepositoryConstant.java
@@ -1,4 +1,90 @@
 package com.openclassrooms.safetynet.constant;
 
 public class PersonRepositoryConstant {
+
+    public static final String PERSON_CSV_CONFIG_FILE = "src/main/resources/csv/persons.csv";
+    public static final String PERSON_List_EMPTY = "Person list is empty, loading data...";
+    public static final String PERSON_LOADED = "Successfully loaded {} persons.";
+    public static final String PERSON_NOT_FOUND = "No persons found in DataBaseInMemoryWrapper.";
+    public static final String PERSON_ERROR_LOADING = "Error loading Json data: {}";
+
+    public static final String PERSON_ERROR_SAVING = "Attempted to save an empty or null person list";
+    public static final String PERSON_SAVING_CSV = "Updated the database and saved to CSV with a total of {} persons.";
+    public static final String PERSON_ERROR_SAVING_CSV = "Nothing  persons found in csv file:";
+    public static final String PERSON_SAVING_DATA_BASE = "Successfully registered {} persons in DataBaseInMemoryWrapper.";
+    public static final String PERSON_ERROR_SAVING_DATA_BASE = "Error registering people: {}";
+
+    public static final String PERSON_ERROR = "Attempted to save a null person.";
+    public static final String PERSON_ERROR_SAVING_CSV_FILE = "Nothing  persons found in csv file =:";
+    public static final String PERSON_SAVING_DATA_BASE_SUC = "Successfully registered {} persons in DataBaseInMemoryWrapper. ";
+    public static final String PERSON_ERROR_SAVING_DATA_BASE_ = "Error registering people: {} ";
+
+    public static final String FULL_NAME_NOT_FOUND = "DataBaseInMemoryWrapper persons list is null.";
+    public static final String ERROR_SEARCHING_FULL_NAME = "Error while searching for person by full name: {}";
+
+    public static final String PERSON_ERROR_UPDATING = "Attempted to update a null person.";
+    public static final String PERSON_NOT_FOUND_UPDATING = "Person not found, adding a new person: {} {}";
+    public static final String PERSON_ERROR_UPDATING_SUCCESS = "Person updated successfully: {}";
+    public static final String PERSON_ERROR_SAVING_UPDATING_SUCCESS = "Error updating person: {}";
+
+    public static final String PERSON_ERROR_DELETING = "DataBaseInMemoryWrapper persons list is null. Cannot perform deletion.";
+    public static final String PERSON_DELETING_SUCCESS = "Person {} {} deleted successfully.";
+    public static final String PERSON_ERROR_DELETING_NOT_FOUND = "Person {} {} not found for deletion.";
+    public static final String PERSON_ERROR_DELETING_BY_FULL_NAME = "Error while deleting person by full name: {}";
+
+    public static final String PERSON_ERROR_SEARCHING_ADDRESSES = "Attempted to search with an empty or null addresses list.";
+    public static final String PERSON_ADDRESSES_NOT_FOUND = "Nothing  addresses found:";
+    public static final String ERROR_SEARCHING_ADDRESSES = "Error while searching for persons by addresses: {}";
+
+    public static final String PERSON_ERROR_SEARCHING_ADDRESS = "Attempted to search with an empty or null address list. ";
+    public static final String PERSON_ADDRESS_NOT_FOUND = "Nothing  address found: ";
+    public static final String ERROR_SEARCHING_ADDRESS = "Error while searching for persons by address: {} ";
+
+    public static final String LAST_NAME_NULL = "Attempted to search with a null or blank last name.";
+    public static final String LAST_NAME_NOT_FOUND = "Nothing  lastname found:";
+    public static final String LAST_NAME_ERROR_SEARCHING_ADDRESS = "Error while searching for persons by last name: {}";
+
+    public static final String CITY_IS_NULL = "Attempted to search with a null or blank city.";
+    public static final String CITY_NOT_FOUND = "Nothing  city found:";
+    public static final String PERSON_TOTAL_IN_DATA_BASE = "Total persons in database: {}";
+    public static final String FILTERED_CITY = "Filtered persons for city '{}': {}";
+    public static final String CITY_ERROR_SEARCHING = "Error while searching for persons by city: {}";
+
+    public static final String CALCULATED_AGE = "Cannot calculate age for a null birth date.";
+    public static final String ERROR_CALCULATED_AGE = "Error while calculating age: {}";
+
+    public static final String ERROR_INVALID_CSV = "Invalid CSV line: {}";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
--- src/main/java/com/openclassrooms/safetynet/repository/PersonRepository.java
+++ src/main/java/com/openclassrooms/safetynet/repository/PersonRepository.java
@@ -2,10 +2,11 @@ package com.openclassrooms.safetynet.repository;
 
 import com.openclassrooms.safetynet.dataBaseInMemory.DataBaseInMemoryWrapper;
 import com.openclassrooms.safetynet.model.Person;
+import com.openclassrooms.safetynet.utils.CsvUtils;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -14,72 +15,80 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Repository
+import static com.openclassrooms.safetynet.constant.PersonRepositoryConstant.*;
+
 @Component
+@AllArgsConstructor
 public class PersonRepository {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PersonRepository.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     // Liste des personnes (sera remplie après le chargement des données)
     private final List<Person> persons = new ArrayList<>();
     private final DataBaseInMemoryWrapper dataBaseInMemoryWrapper;
 
 
-    public PersonRepository (DataBaseInMemoryWrapper dataBaseInMemoryWrapper) {
-        this.dataBaseInMemoryWrapper = dataBaseInMemoryWrapper;
-    }
-
     // CRUD
     public List<Person> getPersons() {
         try {
+            // Vérifier si la liste est vide
             if (persons.isEmpty()) {
+                LOGGER.info(PERSON_List_EMPTY);
 
-                LOGGER.info("Person list is empty, loading data...");
+                // Charger les données depuis la base de données en mémoire
                 dataBaseInMemoryWrapper.loadData();
-
-
                 List<Person> loadedPersons = dataBaseInMemoryWrapper.getPersons();
 
-                if (loadedPersons != null) {
+                // Si des données ont été chargées, les écrire dans le fichier CSV
+                if (loadedPersons != null && !loadedPersons.isEmpty()) {
+                    savePersonsToCsv(loadedPersons);
+                    //loadPersonsFromCsv();
                     persons.addAll(loadedPersons);
-                    LOGGER.info("Successfully loaded {} persons.", loadedPersons.size());
+                    LOGGER.info(PERSON_LOADED, loadedPersons.size());
                 } else {
-                    LOGGER.warn("No persons found in DataBaseInMemoryWrapper.");
+                    LOGGER.warn(PERSON_NOT_FOUND);
                 }
             }
+
+            // Retourner une copie de la liste des personnes
             return new ArrayList<>(persons);
         } catch (Exception e) {
-            LOGGER.error("Error loading Json data: {}", e.getMessage(), e);
+            LOGGER.error(PERSON_ERROR_LOADING, e.getMessage(), e);
+            return List.of();
         }
-        return List.of();
     }
 
     public List<Person> saveAll(List<Person> personList) {
         if (personList == null || personList.isEmpty()) {
-            LOGGER.warn("Attempted to save an empty or null person list");
+            LOGGER.warn(PERSON_ERROR_SAVING);
             return new ArrayList<>(persons); // Retourner l'état actuel sans modification
         }
 
         try {
-            persons.addAll(personList);
+            //persons.addAll(personList);
 
             List<Person> wrapperPersons = dataBaseInMemoryWrapper.getPersons();
             if (wrapperPersons != null) {
                 wrapperPersons.addAll(personList);
+                savePersonsToCsv(wrapperPersons);
+                //LOGGER.info("Successfully loaded {} persons.=======>", wrapperPersons.size());
+                //LOGGER.info(PERSON_SAVING_CSV, wrapperPersons.size());
+
             } else {
-                LOGGER.warn("Nothing  persons found:");
+                //LOGGER.warn("Nothing  persons found:");
+                LOGGER.warn(PERSON_ERROR_SAVING_CSV);
             }
 
-            LOGGER.info("Successfully registered {} persons.", personList.size());
+            LOGGER.info(PERSON_SAVING_DATA_BASE, personList.size());
         } catch (Exception e) {
-            LOGGER.error("Error registering people: {}", e.getMessage(), e);
+            LOGGER.error(PERSON_ERROR_SAVING_DATA_BASE, e.getMessage(), e);
         }
         return new ArrayList<>(persons); // Retourner une copie immuable
     }
 
     public Person save(Person person) {
         if (person == null) {
-            LOGGER.warn("Attempted to save a null person.");
+            LOGGER.warn(PERSON_ERROR);
             return null;
         }
 
@@ -89,22 +98,26 @@ public class PersonRepository {
             List<Person> wrapperPersons = dataBaseInMemoryWrapper.getPersons();
             if (wrapperPersons != null) {
                 wrapperPersons.add(person);
+                savePersonsToCsv(wrapperPersons);
+                LOGGER.info(PERSON_SAVING_CSV, wrapperPersons.size());
             } else {
-                LOGGER.warn("DataBaseInMemoryWrapper persons list is null, skipping wrapper update.");
+                LOGGER.warn(PERSON_ERROR_SAVING_CSV_FILE);
             }
 
-            LOGGER.info("Successfully registered person: {}", person);
+            LOGGER.info(PERSON_SAVING_DATA_BASE_SUC, person);
         } catch (Exception e) {
-            LOGGER.error("Error registering a person {}", e.getMessage(), e);
+            LOGGER.error(PERSON_ERROR_SAVING_DATA_BASE_, e.getMessage(), e);
         }
         return person;
     }
 
     public Optional<Person> findByFullName(String firstName, String lastName) {
+
         try {
             List<Person> allPersons = dataBaseInMemoryWrapper.getPersons(); // Récupérer les données du wrapper
+
             if (allPersons == null) {
-                LOGGER.warn("DataBaseInMemoryWrapper persons list is null.");
+                LOGGER.warn(FULL_NAME_NOT_FOUND);
                 return Optional.empty();
             }
 
@@ -112,14 +125,15 @@ public class PersonRepository {
                     .filter(person -> person.getFirstName().equalsIgnoreCase(firstName)
                             && person.getLastName().equalsIgnoreCase(lastName)).findFirst();
         } catch (Exception e) {
-            LOGGER.error("Error while searching for person by full name: {}", e.getMessage(), e);
+            LOGGER.error(ERROR_SEARCHING_FULL_NAME, e.getMessage(), e);
         }
         return Optional.empty();
     }
 
     public Optional<Person> update(Person updatedPerson) {
+
         if (updatedPerson == null) {
-            LOGGER.warn("Attempted to update a null person.");
+            LOGGER.warn(PERSON_ERROR_UPDATING);
             return Optional.empty();
         }
 
@@ -127,9 +141,10 @@ public class PersonRepository {
             Optional<Person> existingPerson = findByFullName(updatedPerson.getFirstName(), updatedPerson.getLastName());
 
             if (existingPerson.isEmpty()) {
-                LOGGER.info("Person not found, adding a new person: {} {}", updatedPerson.getFirstName(), updatedPerson.getLastName());
+                LOGGER.info(PERSON_NOT_FOUND_UPDATING, updatedPerson.getFirstName(), updatedPerson.getLastName());
                 persons.add(updatedPerson);
                 dataBaseInMemoryWrapper.getPersons().add(updatedPerson); // Mise à jour du wrapper
+                savePersonsToCsv(persons);
                 return Optional.of(updatedPerson);
             }
 
@@ -143,10 +158,10 @@ public class PersonRepository {
                 person.setPhone(updatedPerson.getPhone());
             });
 
-            LOGGER.info("Person updated successfully: {}", updatedPerson);
+            LOGGER.info(PERSON_ERROR_UPDATING_SUCCESS, updatedPerson);
             return existingPerson;
         } catch (Exception e) {
-            LOGGER.error("Error updating person: {}", e.getMessage(), e);
+            LOGGER.error(PERSON_ERROR_SAVING_UPDATING_SUCCESS, e.getMessage(), e);
         }
         return Optional.empty();
     }
@@ -154,40 +169,45 @@ public class PersonRepository {
     public Boolean deleteByFullName(String firstName, String lastName) {
         try {
             List<Person> allPersons = dataBaseInMemoryWrapper.getPersons(); // Recover data
+
             if (allPersons == null) {
-                LOGGER.warn("DataBaseInMemoryWrapper persons list is null. Cannot perform deletion.");
+                LOGGER.warn(PERSON_ERROR_DELETING);
                 return false;
             }
 
+            savePersonsToCsv(allPersons);
+
             boolean isDeleted = allPersons.removeIf(person ->
                     person.getFirstName().equalsIgnoreCase(firstName) && person.getLastName().equalsIgnoreCase(lastName));
+            //savePersonsToCsv(allPersons);
 
             if (isDeleted) {
                 persons.removeIf(person ->
                         person.getFirstName().equalsIgnoreCase(firstName) && person.getLastName().equalsIgnoreCase(lastName));
-                LOGGER.info("Person {} {} deleted successfully.", firstName, lastName);
+                LOGGER.info(PERSON_DELETING_SUCCESS, firstName, lastName);
             } else {
-                LOGGER.warn("Person {} {} not found for deletion.", firstName, lastName);
+                LOGGER.warn(PERSON_ERROR_DELETING_NOT_FOUND, firstName, lastName);
             }
 
             return isDeleted;
         } catch (Exception e) {
-            LOGGER.error("Error while deleting person by full name: {}", e.getMessage(), e);
+            LOGGER.error(PERSON_ERROR_DELETING_BY_FULL_NAME, e.getMessage(), e);
         }
         return false;
     }
 
     // NEW ENDPOINT
     public List<Person> findByAddresses(List<String> addresses) {
+
         if (addresses == null || addresses.isEmpty()) {
-            LOGGER.warn("Attempted to search with an empty or null address list.");
+            LOGGER.warn(PERSON_ERROR_SEARCHING_ADDRESSES);
             return List.of();
         }
 
         try {
             List<Person> allPersons = dataBaseInMemoryWrapper.getPersons();
             if (allPersons == null) {
-                LOGGER.warn("Nothing  addresses found:");
+                LOGGER.warn(PERSON_ADDRESSES_NOT_FOUND);
                 return List.of();
             }
 
@@ -195,21 +215,22 @@ public class PersonRepository {
                     .filter(person -> addresses.contains(person.getAddress()))
                     .collect(Collectors.toList());
         } catch (Exception e) {
-            LOGGER.error("Error while searching for persons by addresses: {}", e.getMessage(), e);
+            LOGGER.error(ERROR_SEARCHING_ADDRESSES, e.getMessage(), e);
         }
         return List.of();
     }
 
     public List<Person> findByAddress(String address) {
+
         if (address == null || address.isBlank()) {
-            LOGGER.warn("Attempted to search with a null or blank address.");
+            LOGGER.warn(PERSON_ERROR_SEARCHING_ADDRESS);
             return List.of();
         }
 
         try {
             List<Person> allPersons = dataBaseInMemoryWrapper.getPersons();
             if (allPersons == null) {
-                LOGGER.warn("Nothing  address found:");
+                LOGGER.warn(PERSON_ADDRESS_NOT_FOUND);
                 return List.of();
             }
 
@@ -217,21 +238,22 @@ public class PersonRepository {
                     .filter(person -> person.getAddress().equalsIgnoreCase(address))
                     .collect(Collectors.toList());
         } catch (Exception e) {
-            LOGGER.error("Error while searching for persons by address: {}", e.getMessage(), e);
+            LOGGER.error(ERROR_SEARCHING_ADDRESS, e.getMessage(), e);
         }
         return List.of();
     }
 
     public List<Person> findByLastName(String lastName) {
+
         if (lastName == null || lastName.isBlank()) {
-            LOGGER.warn("Attempted to search with a null or blank last name.");
+            LOGGER.warn(LAST_NAME_NULL);
             return List.of();
         }
 
         try {
             List<Person> allPersons = dataBaseInMemoryWrapper.getPersons();
             if (allPersons == null) {
-                LOGGER.warn("Nothing  lastname found:");
+                LOGGER.warn(LAST_NAME_NOT_FOUND);
                 return List.of();
             }
 
@@ -239,49 +261,68 @@ public class PersonRepository {
                     .filter(person -> person.getLastName().equalsIgnoreCase(lastName))
                     .collect(Collectors.toList());
         } catch (Exception e) {
-            LOGGER.error("Error while searching for persons by last name: {}", e.getMessage(), e);
+            LOGGER.error(LAST_NAME_ERROR_SEARCHING_ADDRESS, e.getMessage(), e);
         }
         return List.of();
     }
 
     public List<Person> findByCity(String city) {
+
         if (city == null || city.isBlank()) {
-            LOGGER.warn("Attempted to search with a null or blank city.");
+            LOGGER.warn(CITY_IS_NULL);
             return List.of();
         }
-
         try {
             List<Person> allPersons = dataBaseInMemoryWrapper.getPersons();
             if (allPersons == null) {
-                LOGGER.warn("Nothing  city found:");
+                LOGGER.warn(CITY_NOT_FOUND);
                 return List.of();
             }
 
-            LOGGER.debug("Total persons in database: {}", allPersons.size());
+            LOGGER.debug(PERSON_TOTAL_IN_DATA_BASE, allPersons.size());
 
             List<Person> filteredPersons = allPersons.stream()
                     .filter(person -> person.getCity().equalsIgnoreCase(city))
                     .collect(Collectors.toList());
 
-            LOGGER.debug("Filtered persons for city '{}': {}", city, filteredPersons.size());
+            LOGGER.debug(FILTERED_CITY, city, filteredPersons.size());
             return filteredPersons;
         } catch (Exception e) {
-            LOGGER.error("Error while searching for persons by city: {}", e.getMessage(), e);
+            LOGGER.error(CITY_ERROR_SEARCHING, e.getMessage(), e);
         }
         return List.of();
     }
 
     public int calculateAge(LocalDate birthDate) {
         if (birthDate == null) {
-            LOGGER.warn("Cannot calculate age for a null birth date.");
+            LOGGER.warn(CALCULATED_AGE);
             return 0;
         }
 
         try {
             return Period.between(birthDate, LocalDate.now()).getYears();
         } catch (Exception e) {
-            LOGGER.error("Error while calculating age: {}", e.getMessage(), e);
+            LOGGER.error(ERROR_CALCULATED_AGE, e.getMessage(), e);
         }
         return 0;
+    }
+
+    private void savePersonsToCsv(List<Person> personsToSave) {
+        CsvUtils.saveToCsv(PERSON_CSV_CONFIG_FILE, personsToSave);
+    }
+
+    private List<Person> loadPersonsFromCsv() {
+        List<Person> collect;
+        collect = CsvUtils.loadFromCsv(PERSON_CSV_CONFIG_FILE, line -> {
+                    String[] parts = line.split(",");
+                    if (parts.length < 7) {
+                        LOGGER.warn(ERROR_INVALID_CSV, line);
+                        return null;
+                    }
+                    return new Person(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]);
+                }).stream()
+                .filter(person -> person != null)
+                .collect(Collectors.toList());
+        return collect;
     }
 }
